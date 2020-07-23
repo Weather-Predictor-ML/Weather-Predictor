8c0afef05c08216843cdcae0e809d0107971b2bb
Hunt092 <hunters.hunt92@gmail.com>
2020-07-23 12:21:17 +0530

    added few comments requirment.txt for all the imported labraries

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ url = "https://www.wunderground.com/history/monthly/in/pune/VAPO"
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html.parser')
 for i in range(5):
-    body = soup.find(class_="columns small-12")
+    body = soup.find(class_="columns small-12") # there are multiple tables of the same class
     print(""" 
+    -------START------
 
-    -----------------------
 
+    -------Prints one table----
     """)
-    print(body)
+    print(body) # printing all the Tables
     print(""" 
 
-    -----------------------
+    --------END-------
     
     """)
