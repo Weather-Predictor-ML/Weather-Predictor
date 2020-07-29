8accc9117f65e66f95c0b502ae8eb5c56fa04862
Hunt092 <hunters.hunt92@gmail.com>
2020-07-29 14:10:02 +0530

    Trying to find a way for page to go to the location and then find the table

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+from selenium.webdriver.common.keys import Keys
 from config import (
     get_web_driver_options,
     get_chrome_web_driver,
@@ -20,15 +22,31 @@ class WUndergroundAPI:
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
         self.driver=get_chrome_web_driver(options)
-        self.set_filter = f"/{filter['state']}/{filter['period']}/"
+        self.set_filter = f"{filter['state']}/{filter['period']}/"
     
     def Run(self):
         print("Starting the script...")
         print(f"Looking for {self.searchTerm}")
         self.GetPage()
-    
-    
+        time.sleep(6)
+        self.driver.quit
 
+    def GetPage(self):
+        self.driver.get(self.baseURL)
+        element = self.driver.find_element_by_id("wuSearch")
+        element.send_keys(self.searchTerm)
+        time.sleep(1)
+        element.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.driver.get(f"{self.driver.current_url}")#.replace('weather',self.set_filter)}
+        time.sleep(10)
+        print("FIND THE LINK")
+        try:
+            self.driver.find_element_by_class_name('subnav subnav-left')
+        except Exception as e:
+            print (e)
+            print("couldnt find link")
 
 if __name__ == '__main__' :
     hi= WUndergroundAPI(NAME,BASE_URL,FILTERS)
+    hi.Run()
